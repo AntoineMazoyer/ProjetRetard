b168d74f6582cf5dea07e6ef919a9d96134e1547
Antoine <antoine.mazoyer@epsi.fr>
2020-02-24 11:11:20 +0100

    oui

--- ProjetRetard/Controllers/HomeController.cs
+++ ProjetRetard/Controllers/HomeController.cs
@@ -1,45 +1,115 @@
-﻿using System;
+﻿using ProjetRetard.DAL;
+using ProjetRetard.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace ProjetRetard.Controllers
 {
-    public class HomeController : Controller
-    {
+    public class HomeController : Controller {
+
+    private ProjetRetardContext db = new ProjetRetardContext();
+
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(Utilisateur utilisateur, string ReturnURL = "")
         {
-            ViewBag.Message = "Your application description page.";
+            string message;
+            var m = db.Utilisateurs.Where(am => am.AdresseMail == utilisateur.AdresseMail).FirstOrDefault();
+            if (m != null)
+            {
+                if (string.Compare(HashingPass.Hash(utilisateur.MotDePasse), m.MotDePasse) == 0)
+                {
+                    var ticket = new FormsAuthenticationTicket(utilisateur.AdresseMail, false, 525600);
+                    string encrypt = FormsAuthentication.Encrypt(ticket);
+                    var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrypt);
+                    cookie.Expires = DateTime.Now.AddMinutes(5000);
 
+                    if (Url.IsLocalUrl(ReturnURL))
+                    {
+                        return Redirect(ReturnURL);
+                    }
+                }
+                else
+                {
+                    message = "NUL !";
+                }
+            }
+            else
+            {
+                message = "NUL !";
+            }
             return View();
         }
 
-        public ActionResult Contact()
+        private bool IsValid(Utilisateur utilisateur)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return (utilisateur.AdresseMail == "lol@lol.fr" && utilisateur.MotDePasse == "lol");
         }
 
+        [AllowAnonymous]
         public ActionResult Inscription()
         {
             return View();
         }
 
-        public ActionResult Connexion()
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Inscription([Bind(Include = "ID,Nom,Prenom,Classe,AdresseMail,MotDePasse")] Utilisateur utilisateur)
         {
-            return View();
+            string message;
+            if (ModelState.IsValid)
+            {
+                #region Email déjà utilisé
+                var isExist = IsEmailExist(utilisateur.AdresseMail);
+                if (isExist)
+                {
+                    ModelState.AddModelError("AdresseMail", "Adresse email déjà utilisé");
+                }
+                #endregion
+
+                #region Hasher mot de passe
+                utilisateur.MotDePasse = HashingPass.Hash(utilisateur.MotDePasse);
+                #endregion
+
+                db.Utilisateurs.Add(utilisateur);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            else
+            {
+                message = "Formulaire invalide";
+                return View();
+            }
         }
 
+        [Authorize]
         public ActionResult FormulaireRetard()
         {
             return View();
         }
+
+        [NonAction]
+        public bool IsEmailExist(string Email)
+        {
+            var v = db.Utilisateurs.Where(am => am.AdresseMail == Email).FirstOrDefault();
+            return v != null;
+        }
+
+        //[NonAction]
+        //public bool IsValidUser(string Email, string Password)
+        //{
+        //    var mail = db.Utilisateurs.Where(m => m.AdresseMail == Email).Single();
+        //    var pass = db.Utilisateurs.Where(p => p.MotDePasse == Password).Single();
+        //}
     }
 }
--- ProjetRetard/Models/Utilisateur.cs
+++ ProjetRetard/Models/Utilisateur.cs
@@ -18,6 +18,7 @@ namespace ProjetRetard.Models
 
         [Required(ErrorMessage = "Vous devez mettre votre prénom")]
         [MaxLength(50)]
+        [Display(Name = "Prénom")]
         public string Prenom { get; set; }
 
         [Required(ErrorMessage = "Vous devez renseigné votre classe")]
@@ -27,10 +28,13 @@ namespace ProjetRetard.Models
         [Required(ErrorMessage = "Veuillez indiquer votre adresse mail")]
         [MaxLength(50)]
         [EmailAddress]
+        [Display(Name = "Adresse Mail")]
         public string AdresseMail { get; set; }
 
         [Required(ErrorMessage = "Vous devez mettre un mot de passe")]
         [MaxLength(50)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
         public string MotDePasse { get; set; }
 
         public virtual ICollection<BilletRetard> BilletsRetards { get; set; }
